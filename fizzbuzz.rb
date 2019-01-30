87f55622f9669da49cd78abde1192c3352472c66
Olivia Auzenne <oauzenne@gmail.com>
2019-01-30 17:03:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 5 == 0 and int % 3 == 0
+  elsif int % 5 == 0 && int % 3 == 0
     return "FizzBuzz"
   else
     return "nil"
